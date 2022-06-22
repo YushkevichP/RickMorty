40ec8873c16820e821f8bea604700c713b9da1ad
Pavel Yushkevich <7146514@gmail.com>
2022-06-22 22:59:33 +0300

    Try to do night mode

--- data/src/main/java/com/example/hm7_cleanarchitecture/data/repository/FavouritesRepositoryImpl.kt
+++ data/src/main/java/com/example/hm7_cleanarchitecture/data/repository/FavouritesRepositoryImpl.kt
@@ -1,30 +1,30 @@
-package com.example.hm7_cleanarchitecture.data.repository
-
-
-import com.example.hm7_cleanarchitecture.data.database.PersonDao
-import com.example.hm7_cleanarchitecture.data.mapper.toDomainModel
-import com.example.hm7_cleanarchitecture.data.mapper.toPersonEntity
-import com.example.hm7_cleanarchitecture.domain.model.Person
-import com.example.hm7_cleanarchitecture.domain.repository.FavouritesRepository
-
-internal class FavouritesRepositoryImpl(
-    private val personDao: PersonDao,
-) : FavouritesRepository {
-
-    override suspend fun getFavourites(): List<Person> {
-        return personDao.getFavourite().map {
-            it.toDomainModel()
-        }
-    }
-
-    override suspend fun addToFavourites(person: Person) {
-        personDao.addToFavourite(person.toPersonEntity())
-    }
-
-    override suspend fun removeFavourites(person: Person) {
-        personDao.removeFromFavourite(person.toPersonEntity())
-    }
-
-
-}
+//package com.example.hm7_cleanarchitecture.data.repository
+//
+//
+//import com.example.hm7_cleanarchitecture.data.database.PersonDao
+//import com.example.hm7_cleanarchitecture.data.mapper.toDomainModel
+//import com.example.hm7_cleanarchitecture.data.mapper.toPersonEntity
+//import com.example.hm7_cleanarchitecture.domain.model.Person
+//import com.example.hm7_cleanarchitecture.domain.repository.FavouritesRepository
+//
+//internal class FavouritesRepositoryImpl(
+//    private val personDao: PersonDao,
+//) : FavouritesRepository {
+//
+//    override suspend fun getFavourites(): List<Person> {
+//        return personDao.getFavourite().map {
+//            it.toDomainModel()
+//        }
+//    }
+//
+//    override suspend fun addToFavourites(person: Person) {
+//        personDao.addToFavourite(person.toPersonEntity())
+//    }
+//
+//    override suspend fun removeFavourites(person: Person) {
+//        personDao.removeFromFavourite(person.toPersonEntity())
+//    }
+//
+//
+//}
 
